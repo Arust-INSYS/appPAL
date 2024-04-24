63d5f68d2dce2e00acb14b9a8b5e1fd8016a7a21
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2024-04-23 22:14:10 -0500

    Creacion del proyecto

---
+++ appPAL/src/main/java/com/kingram/apppal/AppPAL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kingram.apppal;
+
+/**
+ *
+ * @author Arust
+ */
+public class AppPAL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
