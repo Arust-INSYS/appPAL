791543c6147d28678d4ab9924de497d653700606
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2024-04-23 22:35:08 -0500

    Create Persona.java

---
+++ appPAL/src/main/java/model/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Arust
+ */
+public class Persona {
+    private int id_persona;
+    private int per_nombre;
+    private int per_apellido;
+    
+}
